edfbafc0cb22c419cc00d9e39ba306cd288897b3
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2003-08-09 17:14:58 +0000

    Snapshot 20030809
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@691 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall/fallback.sh
+++ Shorewall/fallback.sh
@@ -28,7 +28,7 @@
 #       shown below. Simply run this script to revert to your prior version of
 #       Shoreline Firewall.
 
-VERSION=1.4.6-20030731
+VERSION=1.4.6-20030809
 
 usage() # $1 = exit status
 {

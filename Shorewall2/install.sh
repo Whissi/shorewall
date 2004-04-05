326898c6c5baedd3c32a25a5b4ad538bd71697e5
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2004-04-05 17:41:13 +0000

    Update for Shorewall 2.0.1
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@1242 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall2/install.sh
+++ Shorewall2/install.sh
@@ -22,7 +22,7 @@
 #       Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
 #
 
-VERSION=2.0.1-RC5
+VERSION=2.0.1
 
 usage() # $1 = exit status
 {
--- Shorewall2/uninstall.sh
+++ Shorewall2/uninstall.sh
@@ -26,7 +26,7 @@
 #       You may only use this script to uninstall the version
 #       shown below. Simply run this script to remove Seattle Firewall
 
-VERSION=2.0.1-RC5
+VERSION=2.0.1
 
 usage() # $1 = exit status
 {

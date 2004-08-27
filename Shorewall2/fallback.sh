0875484d8f8387051ef4441fe06dfc5f47c95d99
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2004-08-27 22:16:22 +0000

    Update for 2.1.7
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@1586 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall2/fallback.sh
+++ Shorewall2/fallback.sh
@@ -28,7 +28,7 @@
 #       shown below. Simply run this script to revert to your prior version of
 #       Shoreline Firewall.
 
-VERSION=2.1.6
+VERSION=2.1.7
 
 usage() # $1 = exit status
 {
--- Shorewall2/install.sh
+++ Shorewall2/install.sh
@@ -22,7 +22,7 @@
 #       Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
 #
 
-VERSION=2.1.6
+VERSION=2.1.7
 
 usage() # $1 = exit status
 {

b51d78b21dd837838715c9f32793bdde3cd6ae22
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2006-10-29 17:30:55 +0000

    Correct build errors with lib.cli -- take 2
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@4758 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall-lite/install.sh
+++ Shorewall-lite/install.sh
@@ -294,7 +294,7 @@ echo "Default config path file installed as ${PREFIX}/usr/share/shorewall-lite/c
 for f in lib.* ; do
     if [ -f $f ]; then
 	install_file $f ${PREFIX}/usr/share/shorewall/$f 0644
-	echo "Library ${f#*.} file installed as ${PREFIX}/usr/share/shorewall/$f"
+	echo "Library ${f#*.} file installed as ${PREFIX}/usr/share/shorewall-lite/$f"
     fi
 done
 

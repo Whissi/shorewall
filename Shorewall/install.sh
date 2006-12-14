134ade3c415fec981a407253a5dc4cc64448a8cf
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2006-12-14 23:05:59 +0000

    Fix file mode of man pages
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@5127 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall/install.sh
+++ Shorewall/install.sh
@@ -678,13 +678,13 @@ cd manpages
 
 for f in *.5; do
     gzip $f
-    run_install -D $f.gz ${PREFIX}/usr/share/man/man5/$f.gz
+    run_install -D  -m 0444 $f.gz ${PREFIX}/usr/share/man/man5/$f.gz
     echo "Man page $f.gz installed to /usr/share/man/man5/$f.gz"
 done
 
 for f in *.8; do
     gzip $f
-    run_install -D $f.gz ${PREFIX}/usr/share/man/man8/$f.gz
+    run_install -D  -m 0444 $f.gz ${PREFIX}/usr/share/man/man8/$f.gz
     echo "Man page $f.gz installed to /usr/share/man/man8/$f.gz"
 done
 

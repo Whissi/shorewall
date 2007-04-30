3156e2f3d2b0bf96bfc2e828f836de887eddcbd1
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2007-04-30 22:25:34 +0000

    Correct error message
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@6166 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall-perl/Shorewall/Config.pm
+++ Shorewall-perl/Shorewall/Config.pm
@@ -825,7 +825,7 @@ sub get_configuration( $ ) {
 	    $config{IPTABLES} = mywhich 'iptables';
 	    fatal_error "Can't find iptables executable" unless $config{IPTABLES};
 	} else {
-	    fatal_error "\$IPTABLES=$capabilities{IPTABLES} does not exist or is not executable" unless -x $config{IPTABLES};
+	    fatal_error "\$IPTABLES=$config{IPTABLES} does not exist or is not executable" unless -x $config{IPTABLES};
 	}
 
 	load_kernel_modules;

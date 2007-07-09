83736d03574dae22f0d1edb994817ee0e13fa82a
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2007-07-09 14:01:07 +0000

    Update versions in all files
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@6826 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall-common/init.sh
+++ Shorewall-common/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 RCDLINKS="2,S41 3,S41 6,K41"
 #
-#     The Shoreline Firewall (Shorewall) Packet Filtering Firewall - V3.4
+#     The Shoreline Firewall (Shorewall) Packet Filtering Firewall - V4.0
 #
 #     This program is under GPL [http://www.gnu.org/copyleft/gpl.htm]
 #

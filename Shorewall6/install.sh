50793dfea4748f02aa5cd3e84e97aef5c11223fc
teastep <teastep@fbd18981-670d-0410-9b5c-8dc0c1a9a2bb>
2009-02-22 20:46:05 +0000

    Fix ArTourter's Shorewall6 install.sh again :-(
    git-svn-id: https://shorewall.svn.sourceforge.net/svnroot/shorewall/trunk@9501 fbd18981-670d-0410-9b5c-8dc0c1a9a2bb

--- Shorewall6/install.sh
+++ Shorewall6/install.sh
@@ -636,7 +636,7 @@ done
 
 for f in *.8; do
     gzip -c $f > $f.gz
-    run_install -D  -m 0644 $f.gz ${PREFIX}${MANDIR}/man/man8/$f.gz
+    run_install -D  -m 0644 $f.gz ${PREFIX}${MANDIR}/man8/$f.gz
     echo "Man page $f.gz installed to ${PREFIX}${MANDIR}/man8/$f.gz"
 done
 

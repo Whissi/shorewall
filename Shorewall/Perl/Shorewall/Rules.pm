5cb9ff0009ad836d0fb26a23ad2f04ded26cec12
Tom Eastep <teastep@shorewall.net>
2009-08-11 08:31:58 -0700

    Fix 'upnpclient' on required interfaces

--- Shorewall/Perl/Shorewall/Rules.pm
+++ Shorewall/Perl/Shorewall/Rules.pm
@@ -631,10 +631,14 @@ sub add_common_rules() {
 		my $base     = uc chain_base $interface;
 		my $variable = get_interface_gateway $interface;
 
-		add_commands( $chainref, 
-			      qq(if [ -n "\$${base}_IS_USABLE" -a -n "$variable" ]; then) ,
-			      qq(    echo -A $chainref->{name} -i $interface -s $variable -p udp -j ACCEPT >&3) ,
-			      qq(fi) );
+		if ( interface_is_optional $interface ) {
+		    add_commands( $chainref, 
+				  qq(if [ -n "\$${base}_IS_USABLE" -a -n "$variable" ]; then) ,
+				  qq(    echo -A $chainref->{name} -i $interface -s $variable -p udp -j ACCEPT >&3) ,
+				  qq(fi) );
+		} else {
+		    add_commands( $chainref, qq(echo -A $chainref->{name} -i $interface -s $variable -p udp -j ACCEPT >&3) );
+		}
 	    }
 	}
     }

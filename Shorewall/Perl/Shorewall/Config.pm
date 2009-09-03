b6190038ab86e9f0e8172da6db2d4629a627d5fa
Tom Eastep <teastep@shorewall.net>
2009-09-03 08:37:04 -0700

    Prepare for 4.4.1.1

--- Shorewall/Perl/Shorewall/Config.pm
+++ Shorewall/Perl/Shorewall/Config.pm
@@ -327,7 +327,7 @@ sub initialize( $ ) {
 		    TC_SCRIPT => '',
 		    EXPORT => 0,
 		    UNTRACKED => 0,
-		    VERSION => "4.4.1",
+		    VERSION => "4.4.1.1",
 		    CAPVERSION => 40401 ,
 		  );
 

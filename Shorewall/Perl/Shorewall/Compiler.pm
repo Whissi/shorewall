5fc0137a2e1dfe676146d446b148396f9ae797fe
Tom Eastep <teastep@shorewall.net>
2009-09-08 17:05:01 -0700

    Update Compiler module version

--- Shorewall/Perl/Shorewall/Compiler.pm
+++ Shorewall/Perl/Shorewall/Compiler.pm
@@ -43,7 +43,7 @@ use Shorewall::Raw;
 our @ISA = qw(Exporter);
 our @EXPORT = qw( compiler EXPORT TIMESTAMP DEBUG );
 our @EXPORT_OK = qw( $export );
-our $VERSION = '4.4_1';
+our $VERSION = '4.4_2';
 
 our $export;
 

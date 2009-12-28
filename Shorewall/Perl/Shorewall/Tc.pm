efcaee16605ee1c73f1c8555a8383c726b5c46aa
Tom Eastep <teastep@shorewall.net>
2009-12-27 16:42:22 -0800

    Tweak new TC

--- Shorewall/Perl/Shorewall/Tc.pm
+++ Shorewall/Perl/Shorewall/Tc.pm
@@ -479,9 +479,9 @@ sub process_simple_device() {
     my $i = 0;
 
     while ( ++$i <= 3 ) {
-	emit "run_tc qdisc add dev $physical parent $number:$i handle ${number}${i}: sfq";
+	emit "run_tc qdisc add dev $physical parent $number:$i handle ${number}${i}: sfq quantum 1500 limit 127 perturb 10";
 	emit "run_tc filter add dev $physical protocol all parent $number: handle $i fw classid $devnum:$i";
-	emit "run_tc filter add dev $physical protocol all parent ${number}$i: handle ${number}${i} flow hash keys $type divisor 1024" if $type ne '-';
+	emit "run_tc filter add dev $physical protocol all prio 1 parent ${number}$i: handle ${number}${i} flow hash keys $type divisor 1024" if $type ne '-';
 	emit '';
     }
 

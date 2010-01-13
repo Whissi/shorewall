b87059157b8207a115d1d6282d32d93ededdbcf7
Tom Eastep <teastep@shorewall.net>
2010-01-13 12:56:06 -0800

    Use TRACK_PROVIDERS to condition tracked packets and PREROUTING
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Tc.pm
+++ Shorewall/Perl/Shorewall/Tc.pm
@@ -1391,7 +1391,18 @@ sub setup_tc() {
 
 	my $mark_part = '';
 
-	$mark_part = '-m mark --mark 0/' . in_hex( $globals{PROVIDER_MASK} ) . ' ' if @routemarked_interfaces && ! $config{TC_EXPERT};
+	if ( @routemarked_interfaces && ! $config{TC_EXPERT} ) {
+	    $mark_part = '-m mark --mark 0/' . in_hex( $globals{PROVIDER_MASK} ) . ' ';
+
+	    unless ( $config{TRACK_PROVIDERS} ) {
+		#
+		# This is overloading TRACK_PROVIDERS a bit but sending tracked packets through PREROUTING is a PITA for users
+		#
+		for my $interface ( @routemarked_interfaces ) {
+		    add_rule $mangle_table->{PREROUTING} , match_source_dev( $interface ) . "-j tcpre";
+		}
+	    }
+	}
 
 	add_jump $mangle_table->{PREROUTING} , 'tcpre', 0, $mark_part;
 	add_jump $mangle_table->{OUTPUT} ,     'tcout', 0, $mark_part;

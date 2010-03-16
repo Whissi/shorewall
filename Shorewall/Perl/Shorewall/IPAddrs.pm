b2a56cd5425b4910bcb54b485f3ec885133a2424
Tom Eastep <teastep@shorewall.net>
2010-03-16 09:53:51 -0700

    Copyright update
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/IPAddrs.pm
+++ Shorewall/Perl/Shorewall/IPAddrs.pm
@@ -109,7 +109,6 @@ use constant { ALLIPv4             => '0.0.0.0/0' ,
 
 our @rfc1918_networks = ( "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16" );
 
-
 #
 # Note: initialize() is declared at the bottom of the file
 #

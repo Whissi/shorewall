0d1f5bf26153bf738e32df0397dd6412da6e5778
Tom Eastep <teastep@shorewall.net>
2010-04-15 14:14:13 -0700

    Use '-j' rather than '-g' when jumping to tcpre, just to be safe
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Tc.pm
+++ Shorewall/Perl/Shorewall/Tc.pm
@@ -1374,7 +1374,7 @@ sub setup_tc() {
 		# This is overloading TRACK_PROVIDERS a bit but sending tracked packets through PREROUTING is a PITA for users
 		#
 		for my $interface ( @routemarked_interfaces ) {
-		    add_jump $mangle_table->{PREROUTING} , 'tcpre', 1, match_source_dev( $interface );
+		    add_jump $mangle_table->{PREROUTING} , 'tcpre', 0, match_source_dev( $interface );
 		}
 	    }
 	}

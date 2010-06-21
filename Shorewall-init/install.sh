6d6b8cf7d664f55fdad6b4d2eff421b231aebdb4
Tom Eastep <teastep@shorewall.net>
2010-06-21 13:32:47 -0700

    Fix crazy Debian start priority for shorewall-init

--- Shorewall-init/install.sh
+++ Shorewall-init/install.sh
@@ -285,7 +285,7 @@ fi
 if [ -z "$DESTDIR" ]; then
     if [ -n "$first_install" ]; then
 	if [ -n "$DEBIAN" ]; then
-	    ln -sf ../init.d/shorewall-init /etc/rcS.d/S38shorewall-init
+	    ln -sf ../init.d/shorewall-init /etc/rcS.d/S08shorewall-init
 	    echo "Shorewall Init will start automatically at boot"
 	else
 	    if [ -x /sbin/insserv -o -x /usr/sbin/insserv ]; then
@@ -319,7 +319,7 @@ else
 		mkdir -p ${DESTDIR}/etc/rcS.d
 	    fi
 
-	    ln -sf ../init.d/shorewall-init ${DESTDIR}/etc/rcS.d/S38shorewall-init
+	    ln -sf ../init.d/shorewall-init ${DESTDIR}/etc/rcS.d/S08shorewall-init
 	    echo "Shorewall Init will start automatically at boot"
 	fi
     fi

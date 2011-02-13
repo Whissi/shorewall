46a99a7cd947dec5a69256c0786e8cbb643d265b
Tom Eastep <teastep@shorewall.net>
2011-02-13 11:46:56 -0800

    Correct Config.pm version again

--- Shorewall/Perl/Shorewall/Config.pm
+++ Shorewall/Perl/Shorewall/Config.pm
@@ -365,7 +365,7 @@ sub initialize( $ ) {
 		    EXPORT     => 0,
 		    STATEMATCH => '-m state --state',
 		    UNTRACKED  => 0,
-		    VERSION    => 4.4.18-Beta2,
+		    VERSION    => "4.4.18-Beta2",
 		    CAPVERSION => 40417 ,
 		  );
     #

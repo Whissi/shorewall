1bcba8bbc7c8a9738860f9b8faa256edb2a665ff
Tom Eastep <teastep@shorewall.net>
2011-04-19 10:02:29 -0700

    Update version of changed Perl modules
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Nat.pm
+++ Shorewall/Perl/Shorewall/Nat.pm
@@ -36,7 +36,7 @@ use strict;
 our @ISA = qw(Exporter);
 our @EXPORT = qw( setup_masq setup_nat setup_netmap add_addresses );
 our @EXPORT_OK = ();
-our $VERSION = '4.4_17';
+our $VERSION = '4.4_20';
 
 our @addresses_to_add;
 our %addresses_to_add;

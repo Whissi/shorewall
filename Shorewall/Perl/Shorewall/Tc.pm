d2407cb7a076a599ea703ec40079ba34281efc92
Tom Eastep <teastep@shorewall.net>
2011-05-02 07:23:28 -0700

    Don't allow IFB classes in tcrules

--- Shorewall/Perl/Shorewall/Tc.pm
+++ Shorewall/Perl/Shorewall/Tc.pm
@@ -257,6 +257,7 @@ sub process_tc_rule( ) {
 	    if ( $config{TC_ENABLED} eq 'Internal' || $config{TC_ENABLED} eq 'Shared' ) {
 		$originalmark =~ s/0x//g;
 		fatal_error "Unknown Class ($originalmark)}" unless ( $device = $classids{$originalmark} );
+		fatal_error "IFB Classes may not be specified in tcrules" if @{$tcdevices{$device}{redirected}};
 	    }
 
 	    $chain   = 'tcpost';

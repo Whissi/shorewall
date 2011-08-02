679de4ccf64067047c4b1c216ff02f798ac9c58c
Tom Eastep <teastep@shorewall.net>
2011-08-02 16:51:49 -0700

    Apply Orion Poplawski's 'qtnoin' patch
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Config.pm
+++ Shorewall/Perl/Shorewall/Config.pm
@@ -2315,7 +2315,7 @@ sub qt( $ ) {
 	print "SYS----> @_\n";
 	system( "@_ 2>&1" );
     } else {
-	system( "@_ > /dev/null 2>&1" ) == 0;
+	system( "@_ > /dev/null 2>&1 < /dev/null" ) == 0;
     }
 }
 

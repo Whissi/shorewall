299701e466934e0df920f8e85d5fd4e61f1482c7
Tom Eastep <teastep@shorewall.net>
2011-12-04 10:21:33 -0800

    Make /sbin/shorewall6 a symbolic link
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall6/install.sh
+++ Shorewall6/install.sh
@@ -276,29 +276,15 @@ echo "Installing Shorewall6 Version $VERSION"
 #
 # Check for /sbin/shorewall6
 #
-if [ -f ${DESTDIR}/sbin/shorewall6 ]; then
+if [ -f ${DESTDIR}/sbin/shorewall6 -o -h ${DESTDIR}/sbin/shorewall6 ]; then
     first_install=""
 else
     first_install="Yes"
 fi
 
-if [ -z "$CYGWIN" ]; then
-   install_file shorewall6 ${DESTDIR}/sbin/shorewall6 0755 ${DESTDIR}/var/lib/shorewall6-${VERSION}.bkout
-   if [ -z "$MACHOST" ]; then
-       eval sed -i \'s\|g_libexec=.\*\|g_libexec=$LIBEXEC\|\' ${DESTDIR}/sbin/shorewall6
-       eval sed -i \'s\|g_perllib=.\*\|g_perllib=$PERLLIB\|\' ${DESTDIR}/sbin/shorewall6
-   else
-       eval sed -i \'\' -e \'s\|g_libexec=.\*\|g_libexec=$LIBEXEC\|\' ${DESTDIR}/sbin/shorewall6
-       eval sed -i \'\' -e \'s\|g_perllib=.\*\|g_perllib=$PERLLIB\|\' ${DESTDIR}/sbin/shorewall6
-   fi
-   echo "shorewall6 control program installed in ${DESTDIR}/sbin/shorewall6"
-else
-   install_file shorewall6 ${DESTDIR}/bin/shorewall6 0755 ${DESTDIR}/var/lib/shorewall6-${VERSION}.bkout
-   eval sed -i \'s\|g_libexec=.\*\|g_libexec=$LIBEXEC\|\' ${DESTDIR}/bin/shorewall6
-   eval sed -i \'s\|g_perllib=.\*\|g_perllib=$PERLLIB\|\' ${DESTDIR}/bin/shorewall6
-   echo "shorewall6 control program installed in ${DESTDIR}/bin/shorewall6"
-fi
+ln -sf shorewall ${DESTDIR}/sbin/shorewall6
 
+echo "/sbin/shorewall6 linked to /sbin/shorewall"
 
 #
 # Install the Firewall Script

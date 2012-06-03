39b3a0da65c3cff7065c50116b5a58cad0db3695
Tom Eastep <teastep@shorewall.net>
2012-06-03 08:43:48 -0700

    Check for conditional directives prior to continuation
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Config.pm
+++ Shorewall/Perl/Shorewall/Config.pm
@@ -1657,8 +1657,14 @@ sub have_capability( $ );
 
 sub process_conditional( $$$ ) {
     my ( $omitting, $line, $linenumber ) = @_;
+    #
+    # Save/set $currentlinenumber so that fatal_error will report correct line number
+    #
+    my $save_currentlinenumber = $currentlinenumber;
 
-    print "CD===> $currentline\n" if $debug;
+    $currentlinenumber = $linenumber;
+
+    print "CD===> $line\n" if $debug;
 
     fatal_error "Invalid compiler directive ($line)" unless $line =~ /^\s*\?(IF\s+|ELSE|ENDIF)(.*)$/;
 
@@ -1710,6 +1716,10 @@ sub process_conditional( $$$ ) {
 	$omitting = $prioromit;
 	pop @ifstack;
     }
+    #
+    # Restore $currentlinenumber
+    #
+    $currentlinenumber = $save_currentlinenumber;
 
     $omitting;
 }
@@ -2269,10 +2279,21 @@ sub read_a_line($) {
 	$currentlinenumber = 0;
 
 	while ( <$currentfile> ) {
+	    chomp;
+	    #
+	    # Handle conditionals
+	    #
+	    if ( /^\s*\?(?:IF|ELSE|ENDIF)/ ) {
+		$omitting = process_conditional( $omitting, $_, $. );
+		next;
+	    }
+
+	    if ( $omitting ) {
+		print "OMIT=> $_\n" if $debug;
+		next;
+	    }
 
 	    $currentlinenumber = $. unless $currentlinenumber;
-
-	    chomp;
 	    #
 	    # Suppress leading whitespace in certain continuation lines
 	    #
@@ -2286,21 +2307,6 @@ sub read_a_line($) {
 	    # Continuation
 	    #
 	    chop $currentline, next if ($currentline .= $_) =~ /\\$/;
-	    #
-	    # Handle conditionals
-	    #
-	    if ( $currentline =~ /^\s*\?(?:IF|ELSE|ENDIF)/ ) {
-		$omitting = process_conditional( $omitting, $currentline, $currentlinenumber );
-		$currentline='';
-		next;
-	    }
-
-	    if ( $omitting ) {
-		print "OMIT=> $currentline\n" if $debug;
-		$currentline='';
-		$currentlinenumber = 0;
-		next;
-	    }
 	    #
 	    # Must check for shell/perl before doing variable expansion
 	    #

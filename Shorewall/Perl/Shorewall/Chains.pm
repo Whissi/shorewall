0753442c3f16f1dafff55835cfe45158d0485802
Tom Eastep <teastep@shorewall.net>
2012-11-23 14:19:14 -0800

    Allow WHITELIST in IPv6
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Chains.pm
+++ Shorewall/Perl/Shorewall/Chains.pm
@@ -2540,6 +2540,7 @@ sub initialize_chain_table($) {
 		    'ULOG'            => STANDARD + LOGRULE + NFLOG,
 		    'ADD'             => STANDARD + SET,
 		    'DEL'             => STANDARD + SET,
+		    'WHITELIST'       => STANDARD,
 		    'HELPER'          => STANDARD + HELPER + NATONLY, #Actually RAWONLY
 		   );
 

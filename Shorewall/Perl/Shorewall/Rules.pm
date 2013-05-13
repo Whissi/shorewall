105d1db85d059078853ea35e6606acae6f190dfc
Tom Eastep <teastep@shorewall.net>
2013-05-13 11:24:09 -0700

    Cosmetic change
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Rules.pm
+++ Shorewall/Perl/Shorewall/Rules.pm
@@ -351,9 +351,7 @@ sub set_policy_chain($$$$$$)
     my $chainref1 = $filter_table->{$chain1};
 
     if ( $chainref ) {
-	if ( $intrazone       &&
-	     $source eq $dest &&
-	     $chainref1->{provisional} ) {
+	if ( $intrazone && $source eq $dest && $chainref1->{provisional} ) {
 	    $chainref1->{policychain} = '';
 	    $chainref1->{provisional} = '';
 	}

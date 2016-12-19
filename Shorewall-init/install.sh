cabef548a651da9294ab936b2b6f270c39ee104d
Tom Eastep <teastep@shorewall.net>
2016-12-19 10:25:30 -0800

    Revert "init: Don't set the 'file' var needlessly"
    This reverts commit 44e9c7780ff075414c671f6e3dc98527f00e73a7.

--- Shorewall-init/install.sh
+++ Shorewall-init/install.sh
@@ -164,8 +164,10 @@ if [ $# -eq 0 ]; then
     #
     if [ -f ./shorewallrc ]; then
 	. ./shorewallrc || exit 1
+	file=./shorewallrc
     elif [ -f ~/.shorewallrc ]; then
 	. ~/.shorewallrc || exit 1
+	file=~/.shorewallrc
      else
 	fatal_error "No configuration file specified and ~/.shorewallrc not found"
     fi
